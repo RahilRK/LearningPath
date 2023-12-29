480d64921f97ba95081efa31ef070f39bcbb8df6
rahil_mithani <rahilmithani007@gmail.com>
2023-12-29 12:16:02 +0530

    - dependency added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-android")
+    id ("kotlin-kapt")
+    id ("androidx.navigation.safeargs.kotlin")
+    id ("dagger.hilt.android.plugin")
+    id ("kotlin-parcelize")
 }
 
 android {
@@ -48,4 +53,31 @@ dependencies {
     /*Coroutines*/
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    //navigation graph and ui
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.0")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.3.0")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.9.0")
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.9.0")
+
+    // alternately - if using Java8, use the following instead of lifecycle-compiler
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.3.1")
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+
+    //HILT DI
+    implementation ("com.google.dagger:hilt-android:2.44")
+    kapt ("com.google.dagger:hilt-android-compiler:2.44")
+
+    /*Retrofit*/
+    implementation ("com.squareup.retrofit2:retrofit:2.6.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.6.0")
+    implementation ("com.squareup.retrofit2:converter-scalars:2.5.0")
+    implementation ("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.5.0")
 }
